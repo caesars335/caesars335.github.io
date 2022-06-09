c1fc259f1d6bb3db64fa6d9cf3c7f28f9c1edf02
tanapun-art <67024496+tanapun-art@users.noreply.github.com>
2022-06-10 02:49:01 +0700

    save

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const app = express();
 const http = require("http").createServer(app);
-// const io = require("socket.io")(http);
-const bcrypt = require('bcrypt');
-const path = require("path");
 const expressLayouts = require('express-ejs-layouts');
-const cookieParser = require('cookie-parser');
-// const mysql = require('mysql2')
-const saltRounds = 10;
-// const config = require("./dbConfig")
-// const con = mysql.createPool(config);
 const bodyParser = require('body-parser');
-const fs = require("fs");
 const port = 3000;
 
 app.use(express.static('public'));
 
-app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
